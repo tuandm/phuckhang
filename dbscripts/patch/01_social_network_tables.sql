24675e28aef805c565a87c114e091d5b0bab8bc1
Duong Thien Duc <duongthienduc@yahoo.com>
2015-01-30 18:05:17 +0700

    Add db scripts to create needed tables of LandBook plugin

---
+++ dbscripts/patch/01_social_network_tables.sql
@@ -0,0 +1,41 @@
+/*Table structure for table `pk_sc_user_activities` */
+
+CREATE TABLE `pk_sc_user_activities` (
+  `sc_user_activity_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `object_id` bigint(20) DEFAULT NULL,
+  `activity_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`sc_user_activity_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `pk_sc_user_friends` */
+
+CREATE TABLE `pk_sc_user_friends` (
+  `sc_user_friend_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned NOT NULL,
+  `friend_id` bigint(20) NOT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`sc_user_friend_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `pk_sc_user_groups` */
+
+CREATE TABLE `pk_sc_user_groups` (
+  `sc_user_groups_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` bigint(20) unsigned NOT NULL COMMENT 'Reference to users.ID',
+  `group_id` bigint(20) unsigned NOT NULL COMMENT 'Reference to terms.term_id',
+  PRIMARY KEY (`sc_user_groups_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `pk_sc_user_photos` */
+
+CREATE TABLE `pk_sc_user_photos` (
+  `sc_user_photo_id` bigint(20) unsigned NOT NULL,
+  `user_id` bigint(20) unsigned DEFAULT NULL,
+  `name` varchar(150) DEFAULT NULL,
+  `path` varchar(250) DEFAULT NULL,
+  `url` varchar(250) DEFAULT NULL,
+  `description` varchar(512) DEFAULT NULL,
+  PRIMARY KEY (`sc_user_photo_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
---
+++ dbscripts/patch/02_create_landbook_project_tables.sql
@@ -0,0 +1,21 @@
+/*Table structure for table `pk_lb_products` */
+
+CREATE TABLE `pk_lb_products` (
+  `lb_product_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `code` varchar(150) DEFAULT NULL,
+  `price` bigint(20) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0' COMMENT '1: deposited, 2: sold, 3: unsold',
+  `lb_project_id` bigint(20) unsigned DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`lb_product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `pk_lb_projects` */
+
+CREATE TABLE `pk_lb_projects` (
+  `lb_project_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(250) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0' COMMENT '1: sold, 2: selling, 3: unsold',
+  `created_date` datetime DEFAULT NULL,
+  PRIMARY KEY (`lb_project_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
