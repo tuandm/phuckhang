f327e182f808befa881833e5c5e75caaf296a78e
Duong Thien Duc <duongthienduc@yahoo.com>
2015-01-30 17:12:25 +0700

    Add posts handling class of LandBook plugin

---
+++ src/wp-content/plugins/landbook/src/LandBook/Posts.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author: Duc Duong
+ */
+class LandBook_Posts extends LandBook_Admin {
+
+    /** Holds the plugin instance */
+    private static $instance = false;
+    /**
+     * Singleton class
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    public function viewAll() {
+        $this->forwardRequestToCI('post', 'viewAll');
+    }
+
+}
