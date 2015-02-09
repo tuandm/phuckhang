84e28dd16c831300bbbc6f117c51ee64ae0a2e17
Phat Nguyen <phatnt2721989@gmail.com>
2015-02-10 02:52:25 +0700

    Fix all tabs to 4 spaces as convention

--- src/ci/application/views/admin/post/view_all.php
+++ src/ci/application/views/admin/post/view_all.php
@@ -1,12 +1,11 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-	/**
-	* Author: Duc Duong
-	*/
+    /**
+    * Author: Duc Duong
+    */
 ?>
 <form method="post">
 <input type="hidden" name="page" value="ttest_list_table">
-	<?php
-	$post->search_box( 'search', 'search_id' );
-	$post->display();
-	echo '</form></div>';
-	
+<?php
+    $post->search_box( 'search', 'search_id' );
+    $post->display();
+    echo '</form></div>';
