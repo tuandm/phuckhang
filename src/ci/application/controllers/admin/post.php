c596bcbfaf2311339374b8c660ee63d0141c07ea
Phat Nguyen <phatnt2721989@gmail.com>
2015-02-17 23:58:27 +0700

    search-filter function

--- src/ci/application/controllers/admin/post.php
+++ src/ci/application/controllers/admin/post.php
@@ -3,20 +3,77 @@
     * User: Duc Duong
     */
 
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+if ( !defined('BASEPATH')) exit('No direct script access allowed');
 
 class Post extends CI_Controller {
     
     public function __construct()
     {
         parent::__construct();
+        $this->load->library('sc_post_manage');
+        $this->load->model('admin/Posts','postModel');
+        $this->load->library('form_validation');
     }
     
     public function index()
     {
-        $this->load->library('sc_post_manage');
-        $post = new CI_SC_Post_Manage();
-        $post->prepare_items();
-        $this->load->view('admin/post/view_all', array('post' => $post));
+        $posts = $this->postModel->getAllPosts();
+        $this->load->view('admin/post/view_all', array('posts' => $posts));
+    }
+    
+    public function edit()
+    {
+        if(!is_admin()) {
+        	die('You dont have permission to edit');
+        };
+        $postId = (int)$this->input->get('post');
+        if($postId <= 0)
+        {
+            echo "Invalid Post ID";
+            return;
+        }
+        $result = $this->postModel->getPostById($postId);
+        $this->load->view('admin/post/edit', array(
+            'post' => $result->posts[0]
+        ));
+    }
+    
+    public function updatePost()
+    {
+        $this->form_validation->set_rules('post-title', 'Title', 'required');
+        $this->form_validation->set_rules('post-content', 'Content', 'required');
+        if($this->form_validation->run()==FALSE) {
+            return $this->edit();
+        } else {
+            $title = $this->input->post('post-title');
+            $content = $this->input->post('post-content');
+            $id = $this->input->post('post-id');
+            $args = array(
+                'ID' => $id,
+                'post_title' => $title,
+                'post_content' => $content
+            );
+            wp_update_post($args);
+            $this->index();
+        }
+    }
+    public function filterAction()
+    {
+        $posts = $this->postModel->getAllPosts();
+        $this->load->view('admin/post/view_all', array('posts' => $posts));
+    }
+    public function delete()
+    {
+        if(!is_admin()) {
+            die('You dont have permission to delete');
+        };
+        $postId = (int)$this->input->get('post');
+        if($postId <= 0)
+        {
+            echo "Invalid Post ID";
+            return;
+        }
+        wp_delete_post($postId);
+        $this->index();
     }
 }
--- src/ci/application/libraries/SC_post_manage.php
+++ src/ci/application/libraries/SC_post_manage.php
@@ -1,10 +1,12 @@
 <?php
-class CI_SC_Post_Manage extends WP_List_Table {
+class MY_SC_Post_Manage extends WP_List_Table
+{
     /**
     * Constructor, we override the parent to pass our own arguments
     * We usually focus on three parameters: singular and plural labels, as well as whether the class supports AJAX.
     */
-    function __construct() {
+    function __construct() 
+    {
         parent::__construct(array(
                         'singular'  => 'sc-post',
                         'plural'    => 'sc-posts',
@@ -16,20 +18,20 @@ class CI_SC_Post_Manage extends WP_List_Table {
     * Add extra markup in the toolbars before or after the list
     * @param string $which, helps you decide if you add the markup after (bottom) or before (top) the list
     */
-    function extra_tablenav($which) {
-        if ($which == 'top') {
-            echo "All posts can find here !!!!";
+    function extra_tablenav($which) 
+    {
+        if ( 'top' == $which ) {
         }
         if ($which == 'bottom') {
             echo "Well Done !!!";
         }
     }
-    
     /**
     * Define the columns that are going to be used in the table
     * @return array $columns, the array of columns to use with the table
     */
-    function get_columns() {
+    function get_columns() 
+    {
         return $columns = array(
                         'cb'    => '<input type="checkbox" />',
                         'col_post_id'   => __('Post Id'),
@@ -43,7 +45,8 @@ class CI_SC_Post_Manage extends WP_List_Table {
     * Decide which columns to activate the sorting functionality on
     * @return array $sortable, the array of columns that can be sorted by the user
     */
-    public function get_sortable_columns() {
+    public function get_sortable_columns() 
+    {
         return $sortable = array(
                         'col_post_id'       => array('ID',false),
                         'col_post_title'    => array('post_title',false),
@@ -51,33 +54,62 @@ class CI_SC_Post_Manage extends WP_List_Table {
                         'col_post_cat'      => array('ID',false)
                         );
     }
-    
-    function get_bulk_actions() {
-        $actions = array(
-                        'delete'    => 'Delete'
-        );
-        return $actions;
-    }
-    function column_cb($item) {
+    /**
+    * Define column checkbox
+    * @return checkbox list
+    */
+    function column_cb($item) 
+    {
         return sprintf(
                 '<input type="checkbox" value="$item->ID" />' 
         );
     }
     
     /**
-    * Prepare the table with different parameters, pagination, columns and table elements
+    * Define column checkbox
+    * @return edit and delete a post
     */
-    function prepare_items() {
-        global $wpdb, $_column_headers;
+    function column_col_post_id($item)
+    {
+        $actions = array(
+                        'edit'      => sprintf('<a href="?page=%s&act=%s&post=%s">Edit</a>',$_REQUEST['page'],'edit',$item->ID),
+                        'delete'    => sprintf('<a href="?page=%s&act=%s&post=%s">Delete</a>',$_REQUEST['page'],'delete',$item->ID),
+        );
+        return sprintf('%1$s %2$s', $item->col_post_id, $this->row_actions($actions) );
+    }
+    
+    /**
+    * A filter for search Action
+    * @return a query
+    */
+    function title_filter( $where, &$wp_query )
+    {
+        global $wpdb;
+        if ($searchTerm = $wp_query->get('searchTitle')) {
+            $where .= ' AND ' . $wpdb->posts . '.post_title LIKE \'%' . esc_sql(like_escape($searchTerm)) . '%\'';
+        }
+        return $where;
+    }
+    /**
+    * Prepare for a post
+    */    
+    function prepare_items() 
+    {
+        global $wpdb, $_column_headers, $cat;
         $screen = get_current_screen();
         $orderBy = !empty($_GET["oderby"]) ? $_GET["oderby"] : 'col_post_id';
         $order = !empty($_GET["order"]) ? $_GET["order"] : 'ASC';
+        $cat = !empty($_POST["cat"]) ? $_POST["cat"] : 0;
+        $postTitle = !empty($_REQUEST["s"]) ? $_REQUEST["s"] : false;
         if(!empty($orderBy) & !empty($order)) { 
             $args = array(
-                            'orderby' => "$orderBy",
-                            'order'   => "$order",
+                            'searchTitle' => $postTitle,
+                            'cat'       => "$cat",
+                            'orderby'   => "$orderBy",
+                            'order'     => "$order",
                             'posts_per_page' =>10
-                    );
+                );
+            add_filter('posts_where', array($this, 'title_filter'), 10, 2);
             $this->items = new WP_Query($args);
         }
         $totalItems = $this->items->found_posts; 
@@ -85,7 +117,7 @@ class CI_SC_Post_Manage extends WP_List_Table {
         $this->set_pagination_args(array(
                         "total_items"   => $totalItems,
                         "total_pages"   => $totalPages,
-                        "per_page"      => 10,
+                        "per_page"      => 10
         ));
         $columns = $this->get_columns();
         $this->_column_headers = array($columns, array(), $this->get_sortable_columns());
@@ -95,7 +127,8 @@ class CI_SC_Post_Manage extends WP_List_Table {
     * Display the rows of records in the table
     * @return string, echo the markup of the rows
     */
-    function display_rows() {
+    function display_rows() 
+    {
         static $row_class = '';
         $row_class = ($row_class == '' ? ' class="alternate"' : '');
         $records = $this->items->posts;
@@ -106,16 +139,21 @@ class CI_SC_Post_Manage extends WP_List_Table {
                 foreach ($columns as $column_name => $column_display_name) {
                     $userData = get_userdata($rec->post_author);
                     $catName = get_the_category($rec->ID)[0]->cat_name;
-                    $class = "class='$column_name column-$column_name'";
+                    $class = "class='$column_name column_$column_name'";
                     $style = "";
                     if (in_array($column_name, $hidden)) {
                         $style = ' style="display:none;"';
                     }
+                    $tmp = (method_exists( $this, 'column_' . $column_name ));
                     $attributes = "$class$style";		
                     $editScPost = '/wp-admin/post.php?action=edit&post=' . (int) $rec->ID;
                     switch ($column_name) {
                         case 'cb' : echo '<th scope="row" class="check-column">' . $this->column_cb($rec) . '</th>'; break;
-                        case 'col_post_id' : echo '<td '.$attributes.'><strong><a href="' . $editScPost . '" title="Edit">' . stripslashes($rec->ID) . '</a></strong></td>'; break;
+                        case 'col_post_id' : echo '<td '.$attributes.'><strong><a href="' . $editScPost . '" title="Edit">' . stripslashes($rec->ID) . '</a></strong>';
+                        if ( method_exists( $this, 'column_' . $column_name ) ) {
+                            echo call_user_func( array( $this, 'column_' . $column_name ), $rec );
+                            echo "</td>";
+                        } break;
                         case 'col_post_title' : echo '<td ' . $attributes . '>' . stripslashes($rec->post_title) . '</td>'; break;
                         case 'col_post_by' : echo '<td ' . $attributes . '>' . stripslashes($userData->user_nicename) . '</td>'; break;
                         case 'col_post_cat' : echo '<td ' . $attributes . '>' . stripslashes($catName) . '</td>'; break;
