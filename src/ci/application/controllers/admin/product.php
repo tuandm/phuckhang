9f5b3da140c807ed67d56782d48336de74ea31c0
Phat Nguyen <phatnt2721989@gmail.com>
2015-03-10 04:36:08 +0700

    Manage product of project

--- src/ci/application/controllers/admin/product.php
+++ src/ci/application/controllers/admin/product.php
@@ -8,18 +8,151 @@ require_once APPPATH."./third_party/PHPExcel/IOFactory.php";
 
 class Product extends CI_Controller {
 
+    private $statusValues = array(0, 1, 2, 3);
+    private $statusNames;
+    private $productTable;
+    private $products;
+
     public function __construct()
     {
         parent::__construct();
-        $this->load->database();
-        $this->load->model('Product_Model', 'productModel');
+        $this->load->library('lb_product_manage');
+        $this->load->model('admin/Products_Model', 'productModel');
         $this->load->library('form_validation');
+        $this->load->database();
         $this->load->helper('url');
+        $this->productTable = new MY_LB_Product_Manage();
         $this->load->library('upload');
+        foreach ($this->statusValues as $value) {
+            $this->statusNames[$value] = $this->productModel->getNameStatusByNumber($value);
+        }
     }
+    
+    /**
+     *
+     */
     public function index()
     {
-        $this->load->view('admin/product/view_all');
+        $msg = '';
+        $orderBy = !empty($this->input->get('orderby')) ? $this->input->get('orderby') : 'code';
+        $order = !empty($this->input->get('order')) ? $this->input->get('order') : 'ASC';
+        $this->products = $this->productModel->getAllProducts($order, $orderBy);
+        $numProduct = $this->productModel->countAllProducts();
+        $this->productTable->prepare_items($this->products, $numProduct);
+        $this->load->view('admin/product/view_all',
+                array(
+                                'productTable'     => $this->productTable,
+                                'msg'           => $msg,
+                                'statusNames'   => $this->statusNames,
+                ));
+    }
+
+    /**
+     *
+     */
+    public function edit()
+    {
+        $statusEditValues = array(1, 2, 3);
+        foreach ($statusEditValues as $value) {
+            $statusEditNames[$value] = $this->productModel->getNameStatusByNumber($value);
+        }
+        $orderBy = !empty($this->input->get('orderBy')) ? $this->input->get('orderBy') : 'code';
+        $order = !empty($this->input->get('order')) ? $this->input->get('order') : 'ASC';
+        if(!is_admin()) {
+            die('You dont have permission to edit');
+        }
+        $productId = $this->input->get('proc');
+        if($productId <= 0) {
+            echo 'Invalid Proj ID';
+            return;
+        }
+        $product = $this->productModel->getProductById($productId);
+        $this->load->view('admin/product/edit', array(
+            'product'       => $product,
+            'statusNames'   => $statusEditNames
+        ));
+    }
+
+    /**
+     *
+     */
+    public function delete()
+    {
+        global $msg;
+        if(!is_admin()) {
+            die('You dont have permission to delete');
+        }
+        $productId = (int)$this->input->get('proc');
+        if($productId <= 0) {
+            echo 'Invalid Product ID';
+            return;
+        }
+        if ($this->productModel->deleteProduct($productId)) {
+            $msg = "Product $productId is deleted";
+            wp_redirect(get_option('siteurl') .'/wp-admin/admin.php?page=landbook-products');
+        } else {
+            echo 'Can not delete this product';
+            return;
+        }
+    }
+
+    /**
+     *
+     */
+    public function update()
+    {
+        $this->form_validation->set_rules('product-code', 'Code', 'required');
+        $this->form_validation->set_rules('price', 'Price', 'required|is_natural_no_zero');
+        $this->form_validation->set_rules('proj-id', 'Project Id', 'required|is_natural_no_zero');
+        if ($this->form_validation->run() == FALSE)
+        {
+            return $this->edit();
+        } else {
+            $code = $this->input->post('product-code');
+            $status = $this->input->post('status');
+            $price = $this->input->post('price');
+            $projId = $this->input->post('proj-id');
+            $productId = $this->input->post('product-id');
+            $product = array(
+                            'lb_product_id'     => $productId,
+                            'code'              => $code,
+                            'status'            => $status,
+                            'price'             => $price,
+                            'lb_project_id'     => $projId,
+            );
+            if ($this->productModel->updateProduct($product)) {
+                wp_redirect(get_option('siteurl') . '/wp-admin/admin.php?page=landbook-products');
+            } else {
+                die('Server is busy');
+            }
+        }
+    }
+
+    /**
+     *
+     */
+    public function filterAction()
+    {
+        $s = $this->input->post('s');
+        $status = $this->input->post('status');
+        $orderBy = !empty($this->input->get('orderBy')) ? $this->input->get('orderBy') : 'code';
+        $order = !empty($this->input->get('order')) ? $this->input->get('order') : 'ASC';
+        $products = $this->productModel->filterProduct($s, $status, $orderBy, $order);
+        $numProduct = $this->productModel->countFilterProduct($s, $status);
+        $productTable = new MY_LB_Product_Manage();
+        $productTable->prepare_items($products, $numProduct);
+        $this->load->view('admin/product/view_all', array(
+            'productTable'  => $productTable,
+            'statusNames'   => $this->statusNames
+        ));
+    }
+
+    /**
+     *
+     */
+    public function importProjects()
+    {
+        $this->load->view('admin/project/import_projects');
     }
 
     public function addProduct()
@@ -27,7 +160,7 @@ class Product extends CI_Controller {
         if ($this->upload->do_upload('myFile'))
         {
             $data = $this->upload->data();
-            //$inputFileName = APPPATH. "./upload/Bang_gia_30_lô.xls";
+            //$inputFileName = APPPATH. "./upload/Bang_gia_30_lÃ´.xls";
             $inputFileName = $data['full_path'];
 
             try {
@@ -48,7 +181,6 @@ class Product extends CI_Controller {
                     TRUE,
                     FALSE);
                 $rowProduct = $rowData[0];
-
                 $product = array (
                     'code' => $rowProduct[1],
                     'price' => $rowProduct[7],
@@ -57,23 +189,23 @@ class Product extends CI_Controller {
                     'area' => $rowProduct[5],
                     'status' => $this->getProductStatus($rowProduct[8])
                 );
-
                 $this->productModel->addProduct($product);
             }
         } else {
             $errors = $this->upload->display_errors();
-            var_dump($errors);
         }
     }
+
     protected function getProductStatus($value)
     {
        switch ($value) {
            case '' :
                return '3';
-           case 'Đã bán' :
+           case 'Ä�Ã£ bÃ¡n' :
                return '2';
            case 'Book' :
                return '1';
        }
     }
+
 }
--- src/ci/application/views/admin/product/view_all.php
+++ src/ci/application/views/admin/product/view_all.php
@@ -1,11 +1,35 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 /**
- * Author: Storm
+ * Author: Phat Nguyen
  */
+?>
+<h2>PRODUCTS MANAGEMENT</h2>
+<div class="alignleft actions">
+<form id="filter" class="filter" action='' method="post">
+    <input type="hidden" id="action" name="act" value="filterAction"><br>
+    <?php
+    if ($this->input->post('status') != 0) {
+        $statusVal = $this->input->post('status');
+    } else {
+        $statusVal = 0;
+    }
+    echo form_dropdown('status', $statusNames , $statusVal);?>
+    <input class="button" type="submit" name="filter_action" value="Filter"/>
+<?php
+    echo '<input type="hidden" name="act" value="filterAction"><br>';
+    $productTable->search_box('search', 'search_id');
+?>
+</form>
+</div>
+<?php
+if (isset($msg) && !empty($msg)) {
+    echo '<script language="javascript">';
+    echo "alert(\"$msg\")";
+    echo '</script>';
+}
+$productTable->display();
 
 ?>
-
-
 <form method="post" action="" enctype="multipart/form-data" />
     <label>Project Name :</label>
     <input type="text" placeholder="Project Name">
