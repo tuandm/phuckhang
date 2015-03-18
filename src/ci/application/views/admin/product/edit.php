a2f63705511b5adae1038465b8465f25921f79d0
Phat Nguyen <phatnt2721989@gmail.com>
2015-03-18 22:50:24 +0700

    add edit product view

---
+++ src/ci/application/views/admin/product/edit.php
@@ -0,0 +1,32 @@
+<?php
+    $this->load->helper('form');
+?>
+<h2>EDIT PRODUCT</h2>
+<form name="edit-product" id="edit-product" method="post" action="<?php echo get_option('siteurl'); ?>/wp-admin/admin.php?page=landbook-products&amp;noheader=true" class="validate" >
+    <table class="form-table">
+        <tbody>
+            <tr class="form-field form-required term-name-wrap">
+                <th scope="row"><label for="product-code">Code<span style="color: #ff0000">(*)</span></label></th>
+                <td><input name="product-code" id="product-code" value="<?php echo $product->code; ?>" size="40" aria-required="true" type="text" required>
+                <span id="validation"><?php echo form_error('product-name'); ?></span>
+            </tr>
+            <tr class="form-field form-required term-name-wrap">
+                <th scope="row"><label for="price">Price<span style="color: #ff0000">(*)</span></label></th>
+                <td><input name="price" id="price" value="<?php echo $product->price; ?>" size="40" aria-required="true" type="text" required>
+                <span id="validation"><?php echo form_error('price'); ?></span>
+            </tr>
+            <tr class="form-field form-required term-name-wrap">
+                <th scope="row"><label for="proj-id">Project<span style="color: #ff0000">(*)</span></label></th>
+                <td><input name="proj-id" id="proj-id" value="<?php echo $product->lb_project_id; ?>" size="40" aria-required="true" type="text" required>
+                <span id="validation"><?php echo form_error('proj-id'); ?></span>
+            </tr>
+                <tr class="form-field term-slug-wrap">
+                <th scope="row"><label for="post-content">Status<span style="color: #ff0000">(*)</span></label></th>
+                <td><?php echo form_dropdown('status', $statusNames ,$product->status); ?></td>
+            </tr>
+        </tbody>
+    </table>
+    <input name="product-id" id="product-id" value="<?php echo $product->lb_product_id; ?>" size="40" aria-required="true" type="hidden">
+    <input type="hidden" id="action" name="act" value="Update"><br>
+    <p class="submit"><input name="update" id="submit" class="button button-primary" value="Update" type="submit"></p>
+</form>
