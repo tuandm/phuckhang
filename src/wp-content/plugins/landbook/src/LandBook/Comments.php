56e108a08b1e9ab4a1f5565a187e1e05021043f5
Storm <phucnguyen19@gmail.com>
2015-03-22 20:00:52 +0700

    BE-07 : Fixed Show comments with panigation & Delete comment.

---
+++ src/wp-content/plugins/landbook/src/LandBook/Comments.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author: Storm
+ */
+class LandBook_Comments extends LandBook_Admin {
+
+    /** Holds the plugin instance */
+    private static $instance = false;
+    /**
+     * Singleton class
+     */
+    public static function getInstance()
+    {
+        if (!self::$instance) {
+            self::$instance = new self();
+        }
+        return self::$instance;
+    }
+
+    protected function getInstanceName() {
+        return 'comment';
+    }
+
+}
