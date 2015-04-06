06dea7e923e2601107b96a83c3f24e46f0ab0325
Phat Nguyen <phatnt2721989@gmail.com>
2015-04-06 21:55:31 +0700

    - Remove refresh default image

--- src/ci/application/views/layout/partial/user_status.php
+++ src/ci/application/views/layout/partial/user_status.php
@@ -6,9 +6,6 @@
         <div class="form-group pull-right">
             <button class="btn btn-primary" name="btnPostStatus" id="btnPostStatus" type="submit">Post</button>
         </div>
-        <div class="refresh" id="user-status-refresh" style="display: none;">
-            <i class="fa fa-3x fa-refresh fa-spin"></i>
-        </div>
         <div id="userStatusError" style="display: none;"></div>
     </form>
 </div>
