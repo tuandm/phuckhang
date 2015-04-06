e776573a6f5684b2eb038f5f24ce411ba353e453
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2015-04-06 23:56:20 +0700

    Repudiate single page template

--- src/wp-content/themes/phuckhang/page.php
+++ src/wp-content/themes/phuckhang/page.php
@@ -4,7 +4,7 @@
 
     <div class="content">
         <div class="row">
-          <div class="col-lg-10 col-md-10 col-md-10 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
+          <div class="col-lg-10 col-md-10 col-sm-10 col-xs-10 col-lg-offset-1 col-md-offset-1 col-sm-offset-1 col-xs-offset-1">
 
             <?php
                 // Start the loop.
