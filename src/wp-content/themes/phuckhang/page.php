6fd26ef332eb28293f616c314bc234f09faf3cae
Tony Tin Nguyen <nguyenquangtin@gmail.com>
2015-04-06 23:47:48 +0700

    Update single page template - page.php

--- src/wp-content/themes/phuckhang/page.php
+++ src/wp-content/themes/phuckhang/page.php
@@ -1,31 +1,21 @@
 <?php
-/**
- * The template for displaying pages
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that
- * other "pages" on your WordPress site will use a different template.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
+    get_header();
+?>
 
-get_header(); ?>
+    <div class="content">
+        <div class="row">
+          <div class="col-lg-10 col-md-10 col-md-10 col-xs-12 col-lg-offset-1 col-md-offset-1 col-sm-offset-1">
 
-    <div id="primary" class="content-area">
-        <main id="main" class="site-main" role="main">
-
-        <?php
-        // Start the loop.
-        while ( have_posts() ) : the_post();
-            // Include the page content template.
-            get_template_part( 'content', 'page' );
-        // End the loop.
-        endwhile;
-        ?>
-
-        </main><!-- .site-main -->
+            <?php
+                // Start the loop.
+                while ( have_posts() ) : the_post();
+                    // Include the page content template.
+                    get_template_part( 'content', 'page' );
+                // End the loop.
+                endwhile;
+            ?>
+            </div>
+        </div>
     </div><!-- .content-area -->
 
 <?php get_footer(); ?>
