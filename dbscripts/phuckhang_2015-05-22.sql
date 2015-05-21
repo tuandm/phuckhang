37ebbfb3aa1ff820c73a1cba40015b6d47af7214
Tuan Duong <bacduong@gmail.com>
2015-05-22 00:37:42 +0700

    Update phuckhang_2015-05-22.sql

--- dbscripts/phuckhang_2015-05-22.sql
+++ dbscripts/phuckhang_2015-05-22.sql
@@ -1250,6 +1250,22 @@ VALUES
 /*!40000 ALTER TABLE `pk_usermeta` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `pk_sc_user_messages`
+--
+
+CREATE TABLE IF NOT EXISTS `pk_sc_user_messages` (
+  `message_id` int(10) NOT NULL AUTO_INCREMENT,
+  `sender_id` int(20) DEFAULT NULL,
+  `receiver_id` int(20) DEFAULT NULL,
+  `sender_name` varchar(45) DEFAULT NULL,
+  `receiver_name` varchar(45) DEFAULT NULL,
+  `message` varchar(1024) DEFAULT NULL,
+  `created_date` datetime DEFAULT NULL,
+  `is_deleted` boolean DEFAULT false,
+  `status` tinyint(1) DEFAULT '0' COMMENT '0: unread, 1: read',
+  PRIMARY KEY (`message_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 # Dump of table pk_users
 # ------------------------------------------------------------
