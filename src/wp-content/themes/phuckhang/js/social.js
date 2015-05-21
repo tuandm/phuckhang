4bd3f21dbdf6f5efc02bfc48203148095530255d
Tuan Duong <tuan.duong@lazada.com>
2015-05-22 00:26:47 +0700

    - Fix dataTable everyone - Add dbscript full

--- src/wp-content/themes/phuckhang/js/social.js
+++ src/wp-content/themes/phuckhang/js/social.js
@@ -295,27 +295,29 @@ function bindReplyMessage()
 }
 
 $(document).ready(function() {
-    $('#products').dataTable({
-        initComplete: function () {
-            this.api().columns().every( function () {
-                var column = this;
-                var select = $('<select><option value="">Tất Cả</option></select>')
-                    .appendTo( $(column.footer()).empty() )
-                    .on( 'change', function () {
-                        var val = $.fn.dataTable.util.escapeRegex(
-                            $(this).val()
-                        );
-                        column
-                            .search( val ? '^'+val+'$' : '', true, false )
-                            .draw();
-                    } );
+    if ($("#products").length > 0) {
+        $('#products').dataTable({
+            initComplete: function () {
+                this.api().columns().every( function () {
+                    var column = this;
+                    var select = $('<select><option value="">Tất Cả</option></select>')
+                        .appendTo( $(column.footer()).empty() )
+                        .on( 'change', function () {
+                            var val = $.fn.dataTable.util.escapeRegex(
+                                $(this).val()
+                            );
+                            column
+                                .search( val ? '^'+val+'$' : '', true, false )
+                                .draw();
+                        } );
 
-                column.data().unique().sort().each( function ( d, j ) {
-                    select.append( '<option value="'+d+'">'+d+'</option>' )
+                    column.data().unique().sort().each( function ( d, j ) {
+                        select.append( '<option value="'+d+'">'+d+'</option>' )
+                    } );
                 } );
-            } );
-        }
-    });
+            }
+        });
+    }
 });
 
 $('.friend_Add').on("click",function(){
